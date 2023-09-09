2605696d60a2bc2ee365b46e40f77f6548ac290e
ykmxxi <ykmxxi97@gmail.com>
2023-09-09 14:39:55 +0900

    스트림 퀴즈: 1.1 각 취미를 선호하는 인원이 몇 명인지 계산

--- stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -1,7 +1,10 @@
 package com.mangkyu.stream.Quiz1;
 
+import static java.util.stream.Collectors.toMap;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +16,12 @@ public class Quiz1 {
 	// 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
 	public Map<String, Integer> quiz1() throws IOException {
 		List<String[]> csvLines = readCsvLines();
-		return new HashMap<>();
+
+		return csvLines.stream()
+					   .map(line -> line[1].trim()) // 취미는 2번 째 값, 공백을 제거
+					   .flatMap(line -> Arrays.stream(line.split(":"))) // 취미는 :로 구분되어 있음, 한 수준으로 평면화(중복 제거)
+					   // key: 취미, value: 선호 인원, mergeFunction: 기존 키 값이 존재하면 새로운 값으로 대체
+					   .collect(toMap(hobby -> hobby, hobby -> 1, (oldVal, newVal) -> newVal += oldVal));
 	}
 
 	// 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
