f3bdaa85aec9e145de39f4d2d30546a0a50cb122
ykmxxi <ykmxxi97@gmail.com>
2023-09-10 15:42:27 +0900

    스트림 퀴즈 2.2: List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환

--- stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -1,5 +1,6 @@
 package com.mangkyu.stream.Quiz2;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toMap;
 
 import java.util.Arrays;
@@ -21,7 +22,11 @@ public class Quiz2 {
 	// 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
 	// ex) ["Hello", "a", "Island", "b"] -> “H I”
 	public String quiz2() {
-		return null;
+		return WORDS.stream()
+					.filter(word -> word.length() > 1) // 길이가 2 이상인 경우만 필터링
+					.map(word -> word.substring(0, 1)) // 접두사 추출
+					.map(String::toUpperCase) // 대문자로 변환
+					.collect(joining(" ")); // 공백(스페이스)로 구분해 하나의 문자열로
 	}
 
 }
