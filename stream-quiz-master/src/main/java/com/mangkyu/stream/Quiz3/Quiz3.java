4ffff6071f7fb2e82f1c09b51829d678cda91185
ykmxxi <ykmxxi97@gmail.com>
2023-09-11 15:24:58 +0900

    스트림 퀴즈 3.2: 모든 숫자 쌍의 곱이 가장 큰 값을 반환

--- stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
+++ stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz3/Quiz3.java
@@ -21,7 +21,13 @@ public class Quiz3 {
 	// 3.2 모든 숫자 쌍의 곱이 가장 큰 값을 반환하여라.
 	// ex) numbers1 = [1,2,3], numbers2 = [3,4] -> 12
 	public int quiz2() {
-		return 0;
+		return numbers1.stream()
+					   .flatMap(n1 -> numbers2.stream()
+											  .map(n2 -> new Integer[] {n1, n2}))
+					   .mapToInt(n -> n[0] * n[1])
+					   .max()
+					   .orElse(0);
+
 	}
 
 }
