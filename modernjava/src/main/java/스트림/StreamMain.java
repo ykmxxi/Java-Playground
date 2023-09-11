6b6bdfdd1e9c40f28257b7f82fc50e478a0263ce
ykmxxi <ykmxxi97@gmail.com>
2023-09-11 14:52:08 +0900

    ch04 스트림 파이프라인 연산 예제

--- modernjava/src/main/java/스트림/StreamMain.java
+++ modernjava/src/main/java/스트림/StreamMain.java
@@ -1,24 +1,38 @@
 package 스트림;
 
+import static 스트림.Dish.Type.FISH;
 import static 스트림.Dish.Type.MEAT;
 import static 스트림.Dish.Type.OTHER;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
 	public static void main(String[] args) {
 		List<Dish> menu = List.of(
-			new Dish("rice", true, 300, OTHER),
-			new Dish("chicken", false, 1400, MEAT),
-			new Dish("pizza", false, 1800, OTHER),
-			new Dish("salad", true, 80, OTHER)
+			new Dish("pork", false, 800, MEAT),
+			new Dish("beef", false, 700, MEAT),
+			new Dish("chicken", false, 400, MEAT),
+			new Dish("french fries", true, 530, OTHER),
+			new Dish("rice", true, 350, OTHER),
+			new Dish("season fruit", true, 120, OTHER),
+			new Dish("pizza", true, 550, OTHER),
+			new Dish("prawns", false, 400, FISH),
+			new Dish("salmon", false, 450, FISH)
 		);
 
 		List<Dish> lowCaloricDishes = Dish.lowCaloricDishes(menu);
 		for (Dish lowCaloricDish : lowCaloricDishes) {
 			System.out.println("lowCaloricDish = " + lowCaloricDish);
 		}
+
+		List<String> threeHighCaloricDishNames = menu.stream()
+													 .filter(dish -> dish.getCalories() > 300)
+													 .map(Dish::getName)
+													 .limit(3)
+													 .collect(Collectors.toList());
+		System.out.println("threeHighCaloricDishNames = " + threeHighCaloricDishNames);
 	}
 
 }
