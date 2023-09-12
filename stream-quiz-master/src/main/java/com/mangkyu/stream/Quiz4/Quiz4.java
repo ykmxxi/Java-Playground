e284bf919e5afb0a36a4c762bec75522595f92bd
ykmxxi <ykmxxi97@gmail.com>
2023-09-12 15:24:35 +0900

    스트림 퀴즈 4.4: 모든 거래자의 이름을 구분자(,)로 구분하여 정렬

--- stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
+++ stream-quiz-master/src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
@@ -1,5 +1,6 @@
 package com.mangkyu.stream.Quiz4;
 
+import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
@@ -56,7 +57,12 @@ public class Quiz4 {
 
 	// 4.4 모든 거래자의 이름을 구분자(",")로 구분하여 정렬하라.
 	public String quiz4() {
-		return null;
+		return transactions.stream()
+						   .map(Transaction::getTrader)
+						   .map(Trader::getName)
+						   .distinct()
+						   .sorted()
+						   .collect(joining(","));
 	}
 
 	// 4.5 부산에 거래자가 있는지를 확인하라.
