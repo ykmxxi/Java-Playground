36eadb9d049e5fb579064cfeaa1b04df35296081
ykmxxi <ykmxxi97@gmail.com>
2023-10-18 17:53:49 +0900

    일급 컬렉션은 컬렉션의 불변(immutable)을 보장

---
+++ modernjava/src/main/java/일급컬렉션/Orders.java
@@ -0,0 +1,33 @@
+package 일급컬렉션;
+
+import java.util.List;
+
+class Order {
+
+	private final long amount;
+
+	public Order(long amount) {
+		this.amount = amount;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+}
+
+public class Orders {
+
+	private final List<Order> orders;
+
+	public Orders(List<Order> orders) {
+		this.orders = orders;
+	}
+
+	public long getTotalAmount() {
+		return orders.stream()
+					 .mapToLong(Order::getAmount)
+					 .sum();
+	}
+
+}
