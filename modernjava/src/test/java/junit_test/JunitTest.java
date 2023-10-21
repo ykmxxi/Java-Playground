e068c741df5349b1f267a1b8e9a8604a7190b42e
ykmxxi <ykmxxi97@gmail.com>
2023-10-21 16:01:07 +0900

    test:  테스트 연습

---
+++ modernjava/src/test/java/junit_test/JunitTest.java
@@ -0,0 +1,26 @@
+package junit_test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class JunitTest {
+
+	@ParameterizedTest
+	@MethodSource("testData")
+	void methodSourceTest(String result, String expected) {
+		assertThat(result).isEqualTo(expected);
+	}
+
+	private static Stream<Arguments> testData() {
+		return Stream.of(
+			Arguments.of("Hello", "Hello"),
+			Arguments.of("World", "World")
+		);
+	}
+
+}
