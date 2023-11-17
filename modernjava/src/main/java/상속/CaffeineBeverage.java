fb3e2526fb873ebd8cb2b18739b289e4abb354af
ykmxxi <ykmxxi97@gmail.com>
2023-11-17 17:14:22 +0900

    add: 1 단계, 중복 코드를 모아 클래스로 분리해 상속

---
+++ modernjava/src/main/java/상속/CaffeineBeverage.java
@@ -0,0 +1,13 @@
+package 상속;
+
+public class CaffeineBeverage {
+
+    public void boilWater() {
+        System.out.println("물을 끓인다");
+    }
+
+    public void pourInCup() {
+        System.out.println("컵에 붓는다");
+    }
+
+}
