43665cf98851ed6c3c2c568737e1701cfcbb3881
ykmxxi <ykmxxi97@gmail.com>
2023-12-05 20:46:15 +0900

    feat: Student 클래스 도입

---
+++ java-basic/src/main/java/chap1/ClassStart3.java
@@ -0,0 +1,13 @@
+package chap1;
+
+public class ClassStart3 {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("학생1", 15, 90);
+        Student student2 = new Student("학생2", 16, 80);
+
+        System.out.println("이름: " + student1.name + " 나이: " + student1.age + " 성적: " + student1.grade);
+        System.out.println("이름: " + student2.name + " 나이: " + student2.age + " 성적: " + student2.grade);
+    }
+
+}
---
+++ java-basic/src/main/java/chap1/Student.java
@@ -0,0 +1,15 @@
+package chap1;
+
+public class Student {
+
+    String name;
+    int age;
+    int grade;
+
+    public Student(final String name, final int age, final int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+}
