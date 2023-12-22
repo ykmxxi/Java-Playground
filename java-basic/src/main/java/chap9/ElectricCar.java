c171d676e5b3ac214336adf08e60a0cceec36904
ykmxxi <ykmxxi97@gmail.com>
2023-12-22 14:54:16 +0900

    feat: 메서드 오버라이딩

--- java-basic/src/main/java/chap9/ElectricCar.java
+++ java-basic/src/main/java/chap9/ElectricCar.java
@@ -2,6 +2,11 @@ package chap9;
 
 public class ElectricCar extends Car {
 
+    @Override
+    public void move() {
+        System.out.println("전기차는 빠르게 움직입니다.");
+    }
+
     public void charge() {
         System.out.println("충전합니다.");
     }
