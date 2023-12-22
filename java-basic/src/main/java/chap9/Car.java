4c016efe3b888cd7419150b4d30b1935d0998435
ykmxxi <ykmxxi97@gmail.com>
2023-12-22 14:37:39 +0900

    feat: 상속 관계를 이용한 기능 추가와 클래스 확장

--- java-basic/src/main/java/chap9/Car.java
+++ java-basic/src/main/java/chap9/Car.java
@@ -6,4 +6,9 @@ public class Car {
         System.out.println("차를 이동합니다.");
     }
 
+
+    public void openDoor() {
+        System.out.println("문열 엽니다.");
+    }
+
 }
--- java-basic/src/main/java/chap9/CarMain.java
+++ java-basic/src/main/java/chap9/CarMain.java
@@ -5,11 +5,18 @@ public class CarMain {
     public static void main(String[] args) {
         ElectricCar electricCar = new ElectricCar();
         electricCar.move();
+        electricCar.openDoor();
         electricCar.charge();
 
         GasCar gasCar = new GasCar();
         gasCar.move();
+        gasCar.openDoor();
         gasCar.fillUp();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.openDoor();
+        hydrogenCar.fillHydrogen();
     }
 
 }
