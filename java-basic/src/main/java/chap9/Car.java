d6a107b7f7e7c90d916241b40f506fe136ddcedf
ykmxxi <ykmxxi97@gmail.com>
2023-12-22 11:05:33 +0900

    feat: 상속 기본

---
+++ java-basic/src/main/java/chap9/Car.java
@@ -0,0 +1,9 @@
+package chap9;
+
+public class Car {
+
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+}
---
+++ java-basic/src/main/java/chap9/ElectricCar.java
@@ -0,0 +1,9 @@
+package chap9;
+
+public class ElectricCar extends Car {
+
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+
+}
