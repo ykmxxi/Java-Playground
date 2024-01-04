7ce8d4652683e67ead7561f989f048197db7393b
ykmxxi <ykmxxi97@gmail.com>
2024-01-04 11:47:53 +0900

    feat: 새로운 차량 추가

--- java-basic/src/main/java/chap12/CarMain.java
+++ java-basic/src/main/java/chap12/CarMain.java
@@ -5,6 +5,7 @@ public class CarMain {
     public static void main(String[] args) {
         Driver driver = new Driver();
         driver.setK3Car(new K3Car());
+        driver.setModel3Car(new Model3Car());
 
         driver.drive();
     }
