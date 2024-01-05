27c1a20ec8c4dceca563d33f513768e941c0794b
ykmxxi <ykmxxi97@gmail.com>
2024-01-05 16:41:17 +0900

    refactor(PayType): 스트림 적용

--- java-basic/src/main/java/chap12/problem2/pay/PayType.java
+++ java-basic/src/main/java/chap12/problem2/pay/PayType.java
@@ -1,26 +1,31 @@
 package chap12.problem2.pay;
 
+import java.util.Arrays;
+
 public enum PayType {
 
-    KAKAO("kakao"),
-    NAVER("naver");
+    KAKAO("kakao", new KakaoPay()),
+    NAVER("naver", new NaverPay());
 
     private final String name;
+    private final Pay pay;
 
-    PayType(final String name) {
+    PayType(final String name, final Pay pay) {
         this.name = name;
+        this.pay = pay;
     }
 
     public static Pay findPayment(final String option) {
-        for (PayType value : PayType.values()) {
-            if (KAKAO.name.equals(option)) {
-                return new KakaoPay();
-            }
-            if (NAVER.name.equals(option)) {
-                return new NaverPay();
-            }
-        }
-        return new DefaultPay();
+        return Arrays.stream(PayType.values())
+                .filter(payType -> payType.hasPayType(option))
+                .findAny()
+                .map(payType -> payType.pay)
+                .orElse(new DefaultPay());
+    }
+
+    private boolean hasPayType(final String option) {
+        return Arrays.stream(PayType.values())
+                .anyMatch(payType -> payType.name.equals(option));
     }
 
 }
