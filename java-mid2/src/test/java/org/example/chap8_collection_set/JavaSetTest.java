d48956bf5d7422454861ecaa9c6b2d212cef164b
ykmxxi <ykmxxi97@gmail.com>
2024-07-03 14:04:06 +0900

    example: LinkedHashSet 추가

--- java-mid2/src/test/java/org/example/chap8_collection_set/JavaSetTest.java
+++ java-mid2/src/test/java/org/example/chap8_collection_set/JavaSetTest.java
@@ -3,6 +3,7 @@ package org.example.chap8_collection_set;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -11,66 +12,76 @@ import org.junit.jupiter.api.Test;
 
 public class JavaSetTest {
 
-    private Set<Integer> set;
+    private Set<Integer> hashSet;
+    private Set<Integer> linkedHashSet;
 
     @BeforeEach
     void setUp() {
-        set = new HashSet<>();
+        hashSet = new HashSet<>();
+        linkedHashSet = new LinkedHashSet<>();
     }
 
     @Test
     void 데이터_추가() {
-        addData();
+        addData(hashSet);
 
-        assertThat(set.size()).isEqualTo(6);
-        assertThat(set).contains(1, 2, 5, 8, 14, 99);
+        assertThat(hashSet.size()).isEqualTo(6);
+        assertThat(hashSet).contains(1, 2, 5, 8, 14, 99);
 
         Set<Integer> addAllSet = new HashSet<>();
-        addAllSet.addAll(set);
+        addAllSet.addAll(hashSet);
 
         assertThat(addAllSet.size()).isEqualTo(6);
         assertThat(addAllSet).contains(1, 2, 5, 8, 14, 99);
+
+        // LinkedHashSet은 순서도 유지
+        addData(linkedHashSet);
+        assertThat(linkedHashSet).containsExactly(1, 2, 5, 8, 14, 99);
     }
 
     @Test
     void 데이터_삭제() {
-        addData();
+        addData(hashSet);
 
-        set.remove(1);
-        assertThat(set.containsAll(List.of(2, 5, 8, 14, 99))).isTrue();
-        assertThat(set.contains(1)).isFalse();
+        hashSet.remove(1);
+        assertThat(hashSet.containsAll(List.of(2, 5, 8, 14, 99))).isTrue();
+        assertThat(hashSet.contains(1)).isFalse();
 
-        set.removeAll(List.of(2, 8, 14));
-        assertThat(set.containsAll(List.of(5, 99))).isTrue();
-        assertThat(set).doesNotContain(2, 8, 14);
+        hashSet.removeAll(List.of(2, 8, 14));
+        assertThat(hashSet.containsAll(List.of(5, 99))).isTrue();
+        assertThat(hashSet).doesNotContain(2, 8, 14);
 
-        set.clear();
+        hashSet.clear();
 
-        addData();
-        set.retainAll(List.of(1, 5, 8));
-        assertThat(set.containsAll(List.of(1, 5, 8))).isTrue();
-        assertThat(set).doesNotContain(2, 14, 99);
+        addData(hashSet);
+        hashSet.retainAll(List.of(1, 5, 8));
+        assertThat(hashSet.containsAll(List.of(1, 5, 8))).isTrue();
+        assertThat(hashSet).doesNotContain(2, 14, 99);
+
+        addData(linkedHashSet);
+        linkedHashSet.remove(1);
+        linkedHashSet.remove(8);
+        assertThat(linkedHashSet).containsExactly(2, 5, 14, 99);
     }
 
     @Test
     void 이외_기능들() {
-        addData();
+        addData(hashSet);
 
-        assertThat(set.isEmpty()).isFalse();
+        assertThat(hashSet.isEmpty()).isFalse();
 
-        Integer[] arr = new Integer[set.size()];
-        set.toArray(arr);
+        Integer[] arr = new Integer[hashSet.size()];
+        hashSet.toArray(arr);
         assertThat(arr).contains(1, 2, 5, 8, 14, 99);
     }
 
-    private void addData() {
+    private void addData(final Set<Integer> set) {
         set.add(1);
         set.add(2);
         set.add(5);
         set.add(8);
         set.add(14);
         set.add(99);
-        set.add(1);
     }
-    
+
 }
