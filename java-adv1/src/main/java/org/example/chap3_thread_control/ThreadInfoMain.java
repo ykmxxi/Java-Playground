55ef9c1fa7515c8fd3ec2216f28022d292ac4320
ykmxxi <ykmxxi97@gmail.com>
2024-07-21 12:06:12 +0900

    example: 스레드 기본 정보

---
+++ java-adv1/src/main/java/org/example/chap3_thread_control/ThreadInfoMain.java
@@ -0,0 +1,27 @@
+package org.example.chap3_thread_control;
+
+import static org.example.util.MyLogger.log;
+
+import org.example.chap2_thread_start.HelloThreadImpl;
+
+public class ThreadInfoMain {
+
+    public static void main(String[] args) {
+        Thread mainThread = Thread.currentThread();
+        log("mainThread = " + mainThread);
+        log("mainThread.threadId() = " + mainThread.threadId());
+        log("mainThread.getName() = " + mainThread.getName());
+        log("mainThread.getPriority() = " + mainThread.getPriority());
+        log("mainThread.getThreadGroup() = " + mainThread.getThreadGroup());
+        log("mainThread.getState() = " + mainThread.getState());
+
+        Thread myThread = new Thread(new HelloThreadImpl(), "myThread");
+        log("myThread = " + myThread);
+        log("myThread.threadId() = " + myThread.threadId());
+        log("myThread.getName() = " + myThread.getName());
+        log("myThread.getPriority() = " + myThread.getPriority());
+        log("myThread.getThreadGroup() = " + myThread.getThreadGroup());
+        log("myThread.getState() = " + myThread.getState());
+    }
+
+}
