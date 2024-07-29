fd1c3375e11e164a899e9c2b5f55052f4a304d9d
ykmxxi <ykmxxi97@gmail.com>
2024-07-29 12:06:23 +0900

    example: synchronized 메서드

---
+++ java-adv1/src/main/java/org/example/chap6_sync/BankAccountV2.java
@@ -0,0 +1,36 @@
+package org.example.chap6_sync;
+
+import static org.example.util.MyLogger.log;
+import static org.example.util.ThreadUtils.sleep;
+
+public class BankAccountV2 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV2(final int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public synchronized boolean withdraw(final int amount) {
+        log("거래 시작: " + this.getClass().getSimpleName());
+
+        log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+        if (balance < amount) {
+            log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+            return false;
+        }
+        log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+        sleep(1_000L);
+        balance -= amount;
+        log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+
+}
