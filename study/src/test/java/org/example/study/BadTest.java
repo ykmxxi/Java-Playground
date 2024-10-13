79a8895808bb5b11731b5eb550de787de6b17eb5
ykmxxi <ykmxxi97@gmail.com>
2024-10-13 12:08:34 +0900

    테스트 작성: 테스트를 위한 제품 코드 생성으로 인한 코드 오염

--- study/src/test/java/org/example/study/BadTest.java
+++ study/src/test/java/org/example/study/BadTest.java
@@ -33,10 +33,37 @@ class BadTest {
         assertThat(result).isEqualTo(5); // 기대값 계산에 도메인 로직 유출을 방지
     }
 
+    @Test
+    @DisplayName("코드 오염 문제: 테스트에만 필요한 제품 코드 추가")
+    void codePollution() {
+        Customer customer = new Customer();
+
+        customer.check("VIP");
+
+        assertThat(customer.state()).isTrue();
+    }
+
     private static class Calculator {
 
         public int sum(int preOperand, int postOperand) {
             return preOperand + postOperand;
+        }
+
+    }
+
+    private static class Customer {
+
+        private boolean state = false;
+
+        public void check(String grade) {
+            if (grade.equals("VIP")) {
+                state = true;
+            }
+        }
+
+        // 불필요한 제품 코드 추가: 비공개 정보인 state를 테스트에 확인하려 getter 추가
+        public boolean state() {
+            return state;
         }
 
     }
